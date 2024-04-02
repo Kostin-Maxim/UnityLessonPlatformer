809bd10385b5c0142558eae032b2683dc7f0059f
Костин Максим <maxim.kostin.1999@gmail.com>
2024-04-02 20:07:19 +0500

    Анимация
    Добавил аниматор и анимацию в него
    Запускаю анимацию из кода в PlayerPlatformer

--- Assets/Scripts/PlatformerPlayer.cs
+++ Assets/Scripts/PlatformerPlayer.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class PlatformerPlayer : MonoBehaviour
 {
     public float speed = 1.0f;
+
+    private Animator anim;
     private Rigidbody2D body;
     void Start()
     {
+        anim = GetComponent<Animator>();
         body = GetComponent<Rigidbody2D>();
     }
 
@@ -15,7 +18,12 @@ public class PlatformerPlayer : MonoBehaviour
     void Update()
     {
         float deltaX = Input.GetAxis("Horizontal") * speed;
+        anim.SetFloat("Speed", Mathf.Abs(deltaX));
         Vector2 movement = new Vector2 (deltaX, body.velocity.y);
         body.velocity = movement;
+        if (!Mathf.Approximately(deltaX, 0.0f))
+        {
+            transform.localScale = new Vector3(Mathf.Sign(deltaX), 1, 1);
+        }
     }
 }
